0853c9fdf7b119e24edbdfb5682261e4fb7fd253
Nandan <nandankmrjha@gmail.com>
2020-03-11 19:55:06 +0530

    deploying to heroku

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const config = require('config')
+const config = require("config");
 const cors = require("cors");
 const path = require("path");
-const db = config.get('mongodbURI')
-
+// const db = config.get("mongodbURI");
 
 const app = express();
 
@@ -12,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 //Connect to db
 mongoose
-  .connect(db, {
+  .connect(process.env.MongoDB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
@@ -23,16 +22,16 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/items", require('./routes/api/items'));
-app.use("/users", require('./routes/api/users'));
-app.use("/auth", require('./routes/api/auth'));
+app.use("/api/items", require("./routes/api/items"));
+app.use("/users", require("./routes/api/users"));
+app.use("/auth", require("./routes/api/auth"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+//   );
+// }
 
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
